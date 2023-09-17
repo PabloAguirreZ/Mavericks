f28d9aa95366df619f6db7b268e4ddb4a2daa230
Pablo Aguirre <pablotzo87@gmail.com>
2023-09-17 13:50:59 -0500

    Actualizacion Login

--- JS/login.js
+++ JS/login.js
@@ -1,17 +1,36 @@
 
-const botonSigin = document.getElementById("boton-Sigin");
-const botonLogin = document.getElementById("boton-Login");
+const botonSigin1 = document.getElementById("boton-Sigin1");
+const botonLogin1 = document.getElementById("boton-Login1");
+const botonSigin2 = document.getElementById("boton-Sigin2");
+const botonLogin2 = document.getElementById("boton-Login2");
 const CrearCuenta = document.querySelector(".Sigin");
 const IniciarSesion = document.querySelector(".Login");
 
-botonSigin.addEventListener("click", e =>{
+botonSigin1.addEventListener("click", e =>{
     CrearCuenta.classList.add("hide");
-    IniciarSesion.classList.remove("hide");
+    IniciarSesion.classList.remove("hide")
+    botonLogin1.classList.add("activo");
+    botonSigin1.classList.remove("activo");
 })
 
-botonLogin.addEventListener("click", e =>{
+botonLogin1.addEventListener("click", e =>{
     IniciarSesion.classList.add("hide");
     CrearCuenta.classList.remove("hide");
+    botonSigin1.classList.add("activo");
+    botonLogin1.classList.remove("activo");
 })
 
+botonSigin2.addEventListener("click", e =>{
+    CrearCuenta.classList.add("hide");
+    IniciarSesion.classList.remove("hide");
+    botonLogin2.classList.add("activo");
+    botonSigin2.classList.remove("activo");
+})
+
+botonLogin2.addEventListener("click", e =>{
+    IniciarSesion.classList.add("hide");
+    CrearCuenta.classList.remove("hide");
+    botonSigin2.classList.add("activo");
+    botonLogin2.classList.remove("activo");
+})
 
